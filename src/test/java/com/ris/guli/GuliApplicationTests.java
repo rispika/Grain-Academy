f03cff31a2505118b262c35b17e19f750afab9ea
ris <3382868941@qq.com>
2023-02-02 20:34:51 +0800

    first commit

---
+++ src/test/java/com/ris/guli/GuliApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ris.guli;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GuliApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
